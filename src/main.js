ebaf29516b050e7f6caba1d03453852889329a09
wytxer <wy@bszhct.com>
2022-09-16 20:10:59 +0800

    refactor：按需加载 antd

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue'
+import { Modal, message, notification, Button } from 'ant-design-vue'
 import App from '@/app.vue'
 import router from '@/router'
-import ComponentName from '@/components'
 import LayoutBox from '@/components/layout-box'
 
+import './app.less'
 import '@wytxer/style-utils/lib/common.less'
-import 'ant-design-vue/dist/antd.less'
 
 Vue.config.productionTip = false
 
-Vue.use(Antd)
-Vue.use(ComponentName)
+Vue.prototype.$confirm = Modal.confirm
+Vue.prototype.$message = message
+Vue.prototype.$notification = notification
+Vue.prototype.$info = Modal.info
+Vue.prototype.$success = Modal.success
+Vue.prototype.$error = Modal.error
+Vue.prototype.$warning = Modal.warning
+
+Vue.use(Button)
 Vue.use(LayoutBox)
 
 new Vue({
